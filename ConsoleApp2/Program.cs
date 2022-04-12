574b3067fd3ea03ccd3b7dbb7062574d4f09af38
Leshich <leshicher@mail.ru>
2022-04-12 06:34:17 +0300

    Point 3.3.5

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,10 +11,12 @@ namespace ConsoleApp2
             bool Pet = true;
             double Shoes = 37.5;
 
-            Console.WriteLine("Me name is" + MyName);
+            Console.WriteLine("Me name is " + MyName);
             Console.WriteLine("My age is " + Age);
             Console.WriteLine("Do i have a pet? " + Pet);
             Console.WriteLine("My she size is " + Shoes);
+            Console.WriteLine("InitMaxValue {0}", int.MaxValue);
+            Console.WriteLine("InitMinValue {0}", int.MinValue);
             Console.ReadKey();
         }
     }
